39ea00666cb48158679e86cd0afd5b877f4183a5
Praneeth3717 <praneethchandupatla@gmail.com>
2025-08-07 21:30:02 +0530

    pro

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Auth from './pages/Auth';
 import Home from './pages/Home';
-import PrivateRoute from './routes/PrivateRoute';
+// import PrivateRoute from './routes/PrivateRoute';
 
 const App = () => {
   return (
@@ -9,9 +9,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Auth />} />
         <Route path="/home" element={
-          <PrivateRoute>
             <Home />
-          </PrivateRoute>
           }/>
       </Routes>
     </Router>
