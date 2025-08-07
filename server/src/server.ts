21f7161726fdc00b3dc919543fd61e1b91812e1d
Praneeth3717 <144830750+Praneeth3717@users.noreply.github.com>
2025-08-07 15:26:15 +0530

    Update server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -22,7 +22,7 @@ connectDB(MONGO_URI).then(() => {
   });
 });
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: 'https://karbon-business-assessment.vercel.app/', credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
