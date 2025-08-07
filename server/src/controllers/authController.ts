6b030c92b37764c8d385a20fa472ef9d19eb6847
Praneeth3717 <144830750+Praneeth3717@users.noreply.github.com>
2025-08-07 15:10:12 +0530

    Update authController.ts

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -9,7 +9,7 @@ const generateToken = (userId: string, name: string) => {
 };
 
 export const googleLogin = (_req: Request, res: Response) => {
-  const redirectUri = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.GOOGLE_CLIENT_ID}&redirect_uri=http://localhost:5000/auth/google/callback&response_type=code&scope=openid%20email%20profile&access_type=offline&prompt=consent`;
+  const redirectUri = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.GOOGLE_CLIENT_ID}&redirect_uri=https://karbon-business-assessment.onrender.com/auth/google/callback&response_type=code&scope=openid%20email%20profile&access_type=offline&prompt=consent`;
   res.redirect(redirectUri);
 };
 
