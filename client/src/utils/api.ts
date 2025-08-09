630398e906f3ef06a3789d133a0376392bd2ed72
Praneeth3717 <144830750+Praneeth3717@users.noreply.github.com>
2025-08-09 16:38:37 +0530

    Update api.ts

--- client/src/utils/api.ts
+++ client/src/utils/api.ts
@@ -1,22 +1,10 @@
 import axios from 'axios';
 
-const isLocalhost = window.location.hostname === 'localhost';
-
-export const API_BASE_URL = isLocalhost
-  ? 'http://localhost:5000'
-  : 'https://karbon-business-assessment.onrender.com';
-
-
-
-// const api = axios.create({
-//   baseURL: 'http://localhost:5000',
-//   withCredentials: true,
-// });
+export const API_BASE_URL = 'https://karbon-business-assessment.onrender.com';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
   withCredentials: true,
 });
-
 
 export default api;
